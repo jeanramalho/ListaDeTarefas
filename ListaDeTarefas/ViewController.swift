f5a84babea51cb60f0f8f6ebdbd9550b726bd8f7
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-03-06 11:16:21 -0300

    [feat] - Inicia projeto de lista de tarefas com userDefaults

---
+++ ListaDeTarefas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListaDeTarefas
+//
+//  Created by Jean Ramalho on 06/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
